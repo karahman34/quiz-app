2d0029ad508093fbaa4f659b51b2547a91bf79ce
karahman <jakarahman007@gmail.com>
2020-12-19 08:39:47 +0700

    add packet seeder

---
+++ database/factories/PacketFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Packet;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PacketFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Packet::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $users = User::select('id')->get();
+        $userIds = $users->pluck('id');
+
+
+        return [
+            'user_id' => $userIds->random(),
+            'title' => $this->faker->sentence
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Packet;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-
         $this->call(UserTableSeeder::class);
+        
+        Packet::factory(40)->create();
     }
 }
