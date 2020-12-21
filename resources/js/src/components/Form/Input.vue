885d50603069d867e83acf0f6ba7ca713cdc6e7a
karahman <jakarahman007@gmail.com>
2020-12-21 10:43:20 +0700

    add readonly prop

--- resources/js/src/components/Form/Input.vue
+++ resources/js/src/components/Form/Input.vue
@@ -1,60 +1,68 @@
 <template>
   <div class="relative">
     <template v-if="icon">
-      <span class="absolute left-2 text-gray-500" :class="[icon, dense ? 'text-lg' : 'text-xl']"></span>
+      <span
+        class="absolute left-2 text-gray-500"
+        :class="[icon, dense ? 'text-lg' : 'text-xl']"
+      ></span>
     </template>
 
-    <input 
+    <input
       v-model="localValue"
-      class="rounded shadow-sm w-full border-gray-400 outline-none focus:outline-none" 
-      :class="[dense ? 'py-1' : 'py-2', {'pl-7': icon}]"
-      :type="type" 
-      :placeholder="placeholder" 
-    >
+      class="rounded shadow-sm w-full border-gray-400 outline-none focus:outline-none disabled:cursor-not-allowed disabled:bg-gray-300"
+      :class="[dense ? 'py-1' : 'py-2', { 'pl-7': icon }]"
+      :type="type"
+      :placeholder="placeholder"
+      :disabled="readonly"
+    />
   </div>
 </template>
 
 <script>
-  export default {
-    props: {
-      value: {
-        type: [Number, String],
-        default: '',
-      },
-      placeholder: {
-        type: String,
-        default: null
-      },
-      type: {
-        type: String,
-        default: 'text'
-      },
-      icon: {
-        type: String,
-        default: null
-      },
-      dense: {
-        type: Boolean,
-        default: false
-      }
+export default {
+  props: {
+    value: {
+      type: [Number, String],
+      default: "",
     },
-
-    data() {
-      return {
-        localValue: null
-      }
+    placeholder: {
+      type: String,
+      default: null,
     },
+    type: {
+      type: String,
+      default: "text",
+    },
+    icon: {
+      type: String,
+      default: null,
+    },
+    dense: {
+      type: Boolean,
+      default: false,
+    },
+    readonly: {
+      type: Boolean,
+      default: false,
+    },
+  },
 
-    watch: {
-      value: {
-        immediate: true,
-        handler(val) {
-          this.localValue = val
-        }
+  data() {
+    return {
+      localValue: null,
+    };
+  },
+
+  watch: {
+    value: {
+      immediate: true,
+      handler(val) {
+        this.localValue = val;
       },
-      localValue(val) {
-        this.$emit('input', val)
-      }
-    }
-  }
+    },
+    localValue(val) {
+      this.$emit("input", val);
+    },
+  },
+};
 </script>
